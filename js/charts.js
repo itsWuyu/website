dd56caedc492e60630f985ee3d682dac98c9e305
wuyu <itsWuyu@gmail.com>
2024-03-18 19:07:24 +0100

    UPDATES

--- js/charts.js
+++ js/charts.js
@@ -1,8 +1,12 @@
 // Function to fetch JSON data from file
+let playerDataAll = null;
+
 async function fetchPlayerData() {
-    const response = await fetch('https://roemmling.it/player.json');
-    const data = await response.json();
-    return data;
+    if (!playerDataAll) {
+        const response = await fetch('/player.json');
+        playerDataAll = await response.json();
+    }
+    return playerDataAll;
 }
  // Function to determine bar color based on level
  function getBarColor(level) {
@@ -24,41 +28,125 @@ async function fetchPlayerData() {
 
 async function getMaxEloPlayer(){
     const playersData = await fetchPlayerData();
-    const playerNames = Object.keys(playersData);
-    const eloValues = playerNames.map(player => playersData[player][0].Elo);
+    const playerNames = playersData.map(player => player.Name);
+    const eloValues = playersData.map(player => player.Elo);
     maxEloName.innerText = playerNames[0];
     maxElo.innerText = eloValues[0];
 }
 
 async function generateList(){
     const data = await fetchPlayerData();
-    const usernames = Object.keys(data);
-    usernames.forEach((username, index) => {
-      const playerData = data[username][0]; // Assuming there's only one object per username
-      const listItem = document.createElement("li");
-      listItem.textContent = `${username}: Elo ${playerData.Elo}, Level ${playerData.Level}`;
-      if (index === 0) {
-        listItem.classList.add("first-item");
-      } else if (index === usernames.length - 1) {
-        listItem.classList.add("last-item");
-      }
-      list.appendChild(listItem);
+    const usernames = data.map(player => player.Name);
+    data.forEach((playerData, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${playerData.Name}: Elo ${playerData.Elo}, Level ${playerData.Level}`;
+        if (index === 0) {
+          listItem.classList.add("first-item");
+        } else if (index === data.length - 1) {
+          listItem.classList.add("last-item");
+        }
+        list.appendChild(listItem);
+      });
+}
+
+let selectedPreyName; // Global variable to store the selected prey's ID
+if(Cookies.get('Prey') != null){
+    selectedPreyName = Cookies.get('Prey')
+}
+
+async function generatePreyOptions() {
+    const selectElement = document.getElementById("prey-select");
+    const data = await fetchPlayerData();
+    const preyCookie = Cookies.get('Prey');
+    let defaultSelected = false;
+
+    data.forEach(player => {
+        const option = document.createElement("option");
+        option.value = player.Name;
+        option.textContent = `${player.Name} (Elo: ${player.Elo})`;
+        if (preyCookie && player.Name === preyCookie) {
+            option.selected = true;
+            defaultSelected = true; // Update the flag if an option is selected
+        }
+        selectElement.appendChild(option);
     });
+
+    // If no option has been selected and preyCookie is set, select the first option by default
+    if (!defaultSelected && preyCookie) {
+        selectElement.options[0].selected = true;
+    }
+}
+
+async function generateHunterAndPreyList(){
+    const list = document.getElementById("player-list");
+    list.innerHTML = ''; // Clear existing list
+    
+    const data = await fetchPlayerData();
+    const hunters = []; // Array für die Jäger
+    let prey; // Variable für den Gejagten
+
+    // Auswahl des Gejagten basierend auf der globalen Variable
+    prey = data.find(playerData => playerData.Name === selectedPreyName); 
+
+    // Auswahl der Jäger
+    data.forEach(playerData => {
+        if (playerData.Name !== selectedPreyName) {
+            hunters.push(playerData);
+        }
+    });
+
+    // Anzeigen der Jäger
+    hunters.forEach((hunter, index) => {
+        const listItem = document.createElement("li");
+        const eloDifference =  prey.Elo -hunter.Elo ; // Berechnung des Unterschieds in den Elo-Punkten
+        if(eloDifference > 0){
+            listItem.textContent = `${hunter.Name} braucht noch  ${eloDifference} bis er ${prey.Name} gepackt hat`;
+            list.appendChild(listItem);
+        }
+    });
+    Cookies.set('Hunter', '1', { expires: 365, sameSite: 'None', secure: true });
+    
+}
+
+
+// Beim Laden der Seite die Dropdown-Optionen generieren
+generatePreyOptions();
+
+// Funktion, um die ausgewählte Beute-ID zu speichern
+document.getElementById("prey-select").addEventListener("change", function() {
+    selectedPreyName = this.value;
+    Cookies.set('Prey', selectedPreyName, { expires: 365, sameSite: 'None', secure: true });
+    console.log(selectedPreyName)
+});
+
+
+async function showAllPlayers() {
+    Cookies.set('Only10', '0', { expires: 365, sameSite: 'None', secure: true });
+    createChart(await fetchPlayerData());
+}
+
+async function showLevel10Players() {
+    const allPlayers = await fetchPlayerData();
+    const level10Players = allPlayers.filter(player => player.Level === 10);
+    Cookies.set('Only10', '1', { expires: 365, sameSite: 'None', secure: true });
+    createChart(level10Players);
 }
 
 
 // Function to create chart
-async function createChart() {
-    const playersData = await fetchPlayerData();
+async function createChart(playersData) {
 
     // Extracting player names and Elo values
-    const playerNames = Object.keys(playersData);
-    const eloValues = playerNames.map(player => playersData[player][0].Elo);
-    const backgroundColors = playerNames.map(level => getBarColor(playersData[level][0].Level));
+    const playerNames = playersData.map(player => player.Name);
+    const eloValues = playersData.map(player => player.Elo);
+    const backgroundColors = playersData.map(player => getBarColor(player.Level));
     // Chart.js code to plot the data
    
     const ctx = document.getElementById('eloChart').getContext('2d');
-    const eloChart = new Chart(ctx, {
+    if (window.eloChartInstance) {
+        window.eloChartInstance.destroy(); // Destroy the existing chart instance
+    }
+    window.eloChartInstance = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: playerNames,
@@ -84,8 +172,28 @@ async function createChart() {
                 },
                 x: { 
                     ticks: {
-                        color: "#ffffff",                        
+                        color: "#ffffff",
+                        display: true                        
                     }
+                }
+            },
+            onClick: function(event, elements) {
+                if (elements.length > 0) {
+                    var clickedIndex = elements[0].index;
+                    var data = window.eloChartInstance.data.datasets[0].data;
+                    data.splice(clickedIndex, 1);
+                     // Hide x-axis label for the clicked bar
+                     window.eloChartInstance.data.labels.pop([clickedIndex]);
+                     window.eloChartInstance.options.scales.y = {
+                        min: eloValues[eloValues.length - 1] - 30,
+                        max: eloValues[0],
+                    };
+                    window.eloChartInstance.update();
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false,
                 }
             }
         }
@@ -95,6 +203,19 @@ async function createChart() {
 
 
 // Call function to create chart
-createChart();
+if(Cookies.get('Only10') == '0'){
+    showAllPlayers();
+}
+else if(Cookies.get('Only10') == '1'){
+    showLevel10Players();
+}
+else{
+    showAllPlayers();
+    Cookies.set('Only10', '0', { expires: 365, sameSite: 'None', secure: true });
+}
 getMaxEloPlayer();
-generateList()
+generateList();
+
+if(Cookies.get('Hunter') == 1 &&  Cookies.get('Prey') != null){
+    generateHunterAndPreyList()
+}
